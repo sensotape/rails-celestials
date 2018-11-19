f808e01f03c6597f28e972b0fe782f96268aa129
Marcel Fonseca <marcelsf23br@gmail.com>
2018-11-19 18:52:24 +0100

    created new/create/edit/update methods

--- app/controllers/celestials_controller.rb
+++ app/controllers/celestials_controller.rb
@@ -1,2 +1,34 @@
 class CelestialsController < ApplicationController
+  def new
+    @celestial = Celestial.new
+  end
+
+  def create
+    @celestial = Celestial.new(celestial_params)
+    @celestial.user = current_user
+    if @celestial.save
+      redirect_to user_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @celestial = Celestial.find(params[:id])
+  end
+
+  def update
+    @celestial = Celestial.find(params[:id])
+    if @celestial.update(celestial_params)
+      redirect_to user_path
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def celestial_params
+    params.require(:celestial).permit(:photo, :name, :price, :category, :location)
+  end
 end
