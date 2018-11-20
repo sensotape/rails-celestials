5aad3520c673f8b182fb7b5bf6e7b912f02779e1
May-Lee Sia <mayleesia@gmail.com>
2018-11-20 13:08:26 +0100

    Redirected pages for sign in, sign up and sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource_or_scope)
+    current_user
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    URI.parse(request.referer).path if request.referer
+  end
+
   protected
 
   def configure_permitted_parameters
