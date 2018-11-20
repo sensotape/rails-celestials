829fe272ddb7b21b5285b1839d23a89b41de7d6d
Aigerim Kalysheva <41161878+sensotape@users.noreply.github.com>
2018-11-20 17:34:52 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   root to: 'pages#home'
   resources :celestials, except: :show
-  get 'users/:id', to: 'users#show', as: :user
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update, :show]
   resources :messages, only: [:index, :show]
 end
