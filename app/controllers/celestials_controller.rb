c457fca86d3d1105a5dcf1dc929a8ba649e4570b
Andrew Erlanger <a.erlanger@gmail.com>
2018-11-20 10:44:17 +0100

    added first phase of filter box

--- app/controllers/celestials_controller.rb
+++ app/controllers/celestials_controller.rb
@@ -1,6 +1,20 @@
 class CelestialsController < ApplicationController
   def index
-    @celestials = Celestrial.all
+    @celestials = Celestial.all
+    # filtering_params(params).each do |key, value|
+    #   @celestials = @celestials.public_send(key, value) if value.present?
+    # end
+    @celestial = Celestial.new
+  end
+
+  private
+
+  def celestial_params
+    params.require(:celestial).permit(:category) if params['celestial']
+  end
+
+  def filtering_params(params)
+    params.slice(:category)
   end
 
 end
