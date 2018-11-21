3eb81eef74f3f8b6ff3dbca2f167b6d9502d73a2
marcel fonseca <marcelsf23br@gmail.com>
2018-11-21 16:13:50 +0100

    buttons are now gold colored

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,28 @@
 class MessagesController < ApplicationController
+
+  def new
+    @message = current_user.messages.new
+    authorize @message
+  end
+
+  def create
+    @message = current_user.messages.new(message_params)
+    if @message.save
+
+    else
+
+    end
+
+  end
+
+  private
+
+  current_user.interests
+
+  @message.interest =
+
+  def message_params
+    params.require(:message).permit(:body)
+  end
+
 end
