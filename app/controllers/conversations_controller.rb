683a9488ce5e17d0a3cc89f905ffe97ba9af28ad
Aigerim Kalysheva <sensotape@gmail.com>
2018-11-23 17:35:29 +0100

    some final changes sorry

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -3,21 +3,7 @@ class ConversationsController < ApplicationController
   ERROR = "Sorry! Something went wrong! Try again."
 
   def index
-    # @all_sent_messages = []
-    # @user_interests = policy_scope(Interest)
-    # @user_interests.each do |interest|
-    #   @all_sent_messages << policy_scope(Message).where(interest: interest)
-    # end
-    # @all_recieved_messages = []
-    # @owner_interests = []
-    # @owned_celestials = policy_scope(Celestial).where(user: current_user)
-    # @owned_celestials.each do |celestial|
-    #   @owner_interests << policy_scope(Interest).where(celestial: celestial)
-    # end
-    # @owner_interests.each do |interest|
-    #   @all_recieved_messages << interest.messages if interest
-    # end
-    @conversations = policy_scope(Conversation).where(recipient: current_user) && Conversation.where(sender: current_user)
+    @conversations = policy_scope(Conversation).where(recipient_id: current_user.id) && policy_scope(Conversation).where(sender_id: current_user.id)
   end
 
   def create
@@ -25,7 +11,7 @@ class ConversationsController < ApplicationController
     if @interest.save && @conversation.save
       redirect_to conversations_path, flash: { notice: SUCCESS + @celestial.name }
     else
-      redirect_to celestial_path(interest.celestial), flash: { error: ERROR }
+      redirect_to celestial_path(@celestial), flash: { error: ERROR }
     end
   end
 
